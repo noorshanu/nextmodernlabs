a74ce5f3dc47e466d24a6a06c9d2d45e420eec8b
noor alam <51040978+noorshanu@users.noreply.github.com>
2026-02-15 16:21:59 +0530

    new

---
+++ app/components/home/LeadGen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeadGen = () => {
+  return (
+    <div>LeadGen</div>
+  )
+}
+
+export default LeadGen
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/home/Hero";
 import Services from "./components/home/Services";
@@ -5,8 +8,20 @@ import WhyUs from "./components/home/WhyUs";
 import Process from "./components/home/Process";
 import CTA from "./components/home/CTA";
 import Footer from "./components/layout/Footer";
+import LeadGenPopup from "./components/home/LeadGenPopup";
 
 export default function Home() {
+    const [showPopup, setShowPopup] = useState(false);
+
+    useEffect(() => {
+        // Show popup after 10 seconds
+        const timer = setTimeout(() => {
+            setShowPopup(true);
+        }, 10000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <main className="min-h-screen">
             <Navbar />
@@ -16,6 +31,8 @@ export default function Home() {
             <Process />
             <CTA />
             <Footer />
+
+            <LeadGenPopup isOpen={showPopup} onClose={() => setShowPopup(false)} />
         </main>
     );
 }
